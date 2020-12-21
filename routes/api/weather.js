5f27a935c05ec856d0d4e09a29b4dbec4156a5bd
luke-h1 <luke.howsam@yahoo.com>
2020-12-21 19:59:02 +0000

    add weather route, untested tho

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -3,17 +3,23 @@ const router = express.Router();
 const axios = require('axios');
 const dotenv = require('dotenv');
 
-
 // @route   GET api/weather/:city
-// @desc    GET weather for city from Weather API 
-// @access  PUBLIC 
+// @desc    GET weather for city from Weather API
+// @access  PUBLIC
 
-router.get('/weather/:city', async (req, res) => { 
+router.get('/weather/:city', async (req, res) => {
   try {
     const REQ_URL = encodeURI(
-      `https`
-    )
+      `https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${req.params.city}`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+    };
+    const response = await axios.get(REQ_URL);
+    return res.json();
   } catch (e) {
-    
+    console.error(e.message);
+    return res.status(404).json({ msg: `No city found. Error: ${e.message}` });
   }
-})
+});
+module.exports = router;
