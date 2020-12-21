0e2bbcfb9f4743615a85fe48058a8ae227323250
luke-h1 <luke.howsam@yahoo.com>
2020-12-21 19:49:43 +0000

    setup basic express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.status(200).json({ msg: 'Welcome to the weather API' })
+);
+
+// define routes
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
