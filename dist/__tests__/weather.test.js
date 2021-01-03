d28038cd1858bead535ced1e62d5aeb9fed6ed6d
luke-h1 <luke.howsam@yahoo.com>
2021-01-03 16:53:06 +0000

    add weather tests

--- dist/__tests__/weather.test.js
+++ dist/__tests__/weather.test.js
@@ -10,3 +10,9 @@ test('weather route should return 200', async () => {
   const response = await (0, _supertest.default)(_server.app).get('/api/city/weather/london');
   expect(response.statusCode).toBe(200);
 });
+test('Weather should have keys location & current', async () => {
+  const response = await (0, _supertest.default)(_server.app).get('/api/city/weather/london');
+  const json = await response.json();
+  expect(json.body).toHaveProperty('location');
+  expect(json.body).toHaveProperty('current');
+});
--- src/__tests__/weather.test.js
+++ src/__tests__/weather.test.js
@@ -5,3 +5,9 @@ test('weather route should return 200', async () => {
   const response = await request(app).get('/api/city/weather/london')
   expect(response.statusCode).toBe(200)
 })
+
+test('Weather should have keys location & current', async () => {
+  const response = await request(app).get('/api/city/weather/london')
+  expect(response.body).toHaveProperty('location')
+  expect(response.body).toHaveProperty('current')
+})
