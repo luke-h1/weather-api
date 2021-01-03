948caa138a3c7872b6f69d7cc84981805ac36ce4
luke-h1 <luke.howsam@yahoo.com>
2021-01-03 16:56:33 +0000

    update weather tests

--- src/__tests__/weather.test.js
+++ src/__tests__/weather.test.js
@@ -10,4 +10,12 @@ test('Weather should have keys location & current', async () => {
   const response = await request(app).get('/api/city/weather/london')
   expect(response.body).toHaveProperty('location')
   expect(response.body).toHaveProperty('current')
+  expect(response.body).toHaveProperty('location.region')
+  expect(response.body).toHaveProperty('location.name')
+  expect(response.body).toHaveProperty('location.region')
+  expect(response.body).toHaveProperty('current.condition')
+  expect(response.body).toHaveProperty('current.wind_mph')
+  expect(response.body).toHaveProperty('current.feelslike_c')
+
 })
+
